e36179a9c7930030bfd0434538157b75f28ac839
hyper <hyperskill@hyperskill.org>
2024-01-12 20:31:02 +0600

    Stage 6/6

--- main.js
+++ main.js
@@ -9,22 +9,31 @@ let machineBeans = 120;
 let machineDisposableCups = 9;
 let machineMoney = 550;
 
-showMachineContent();
+let hasFinished = false;
+while (!hasFinished) {
+    let buyFillTake = input("Write action (buy, fill, take, remaining, exit)\n");
 
-let buyFillTake = input("Write action (buy, fill, take)\n");
-switch (buyFillTake) {
-    case "buy":
-        buy();
-        break;
-    case "fill":
-        fill();
-        break;
-    case "take":
-        take();
-        break;
+    switch (buyFillTake) {
+        case "buy":
+            buy();
+            break;
+        case "fill":
+            fill();
+            break;
+        case "take":
+            take();
+            break;
+        case "remaining":
+            showMachineContent();
+            break;
+        case "exit":
+            hasFinished = true;
+            break;
+    }
+    console.log();
 }
 
-showMachineContent();
+
 function showMachineContent() {
 
     console.log(`The coffee machine has:
@@ -35,30 +44,58 @@ ${machineDisposableCups} disposable cups
 $${machineMoney} of money\n`)
 }
 function buy() {
+    console.log();
     let coffeeChoice = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n")
+    console.log();
     switch (coffeeChoice) {
         case "1":
-            machineWater -= 250;
-            machineBeans -= 16;
-            machineDisposableCups -= 1;
-            machineMoney += 4;
+
+            if (machineWater >= 250 && machineBeans >= 16 && machineDisposableCups >= 1) {
+                machineWater -= 250;
+                machineBeans -= 16;
+                machineDisposableCups -= 1;
+                machineMoney += 4;
+                console.log("I have enough resources, making you a coffee!")
+            } else if (machineWater < 250) {
+                console.log("Sorry, not enough water!");
+            } else if (machineBeans < 16) {
+                console.log("Sorry, not enough coffee beans");
+            }
             break;
         case "2":
-            machineWater -= 350;
-            machineMilk -= 75;
-            machineBeans -= 20;
-            machineDisposableCups -= 1;
-            machineMoney += 7;
+            if (machineWater >= 350 && machineMilk >= 75 && machineBeans >= 20 && machineDisposableCups >= 1) {
+                machineWater -= 350;
+                machineMilk -= 75;
+                machineBeans -= 20;
+                machineDisposableCups -= 1;
+                machineMoney += 7;
+                console.log("I have enough resources, making you a coffee!")
+            } else if (machineWater < 350) {
+                console.log("Sorry, not enough water!");
+            } else if (machineMilk < 76) {
+                console.log("Sorry, not enough milk!");
+            } else if (machineBeans < 20) {
+                console.log("Sorry, not enough coffee beans");
+            }
             break;
         case "3":
-            machineWater -= 200;
-            machineMilk -= 100;
-            machineBeans -= 12;
-            machineDisposableCups -= 1;
-            machineMoney += 6;
+            if (machineWater >= 200 && machineMilk >= 100 && machineBeans >= 12 && machineDisposableCups >= 1) {
+                machineWater -= 200;
+                machineMilk -= 100;
+                machineBeans -= 12;
+                machineDisposableCups -= 1;
+                machineMoney += 6;
+                console.log("I have enough resources, making you a coffee!")
+            } else if (machineWater < 200) {
+                console.log("Sorry, not enough water!");
+            } else if (machineMilk < 100) {
+                console.log("Sorry, not enough milk!");
+            } else if (machineBeans < 12) {
+                console.log("Sorry, not enough coffee beans");
+            }
             break;
     }
-    console.log();
+
 }
 
 function fill() {
@@ -74,7 +111,7 @@ function fill() {
 }
 
 function take() {
-    console.log(`I gave you $${550}`);
+    console.log(`I gave you $${machineMoney}`);
     machineMoney = 0;
     console.log();
 }
@@ -89,6 +126,7 @@ function take() {
 // const waterPerCup = 200;
 // const milkPerCup = 50;
 // const beansPerCup = 15;
+// const coffeeCupPerCup = 1;
 //
 // let machineWater = input("Write how many ml of water the coffee machine has:\n");
 // let machineMilk = input("Write how many ml of milk the coffee machine has:\n");
@@ -98,6 +136,7 @@ function take() {
 // let waterPortion = Math.floor(machineWater / waterPerCup);
 // let milkPortion = Math.floor(machineMilk / milkPerCup);
 // let beansPortion = Math.floor(machineBeans / beansPerCup);
+// let
 // let maxNum = Math.min(waterPortion, milkPortion, beansPortion);
 // let secondaryMaxNum = maxNum - cupNum;
 //
